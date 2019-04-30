430820d1bb6dc0659476e1933379b9bcc5500273
brellin <cekakid@gmail.com>
2019-04-30 14:13:19 -0500

    Stretch

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const helmet = require('helmet')
 
 const router = require('./router')
+const bearsRouter = require('./bears')
 
 const server = express()
 
@@ -10,5 +11,6 @@ server.use(express.json());
 server.use(helmet());
 
 server.use('/api/zoos', router)
+server.use('/api/bears', bearsRouter)
 
 module.exports = server
